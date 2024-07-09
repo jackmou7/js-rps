ee29bbc2c457527c74b7415895bad4cd0ddafccf
Jack Mou <jackmou7@gmail.com>
2024-07-08 17:06:10 -0700

    Add default HTML page and link it to JS file

---
+++ rps.js
@@ -0,0 +1,14 @@
+/*
+Pseudocode:
+    Overarching Program: Create a JS program that takes in user input to play rock paper scissors against a computer/the console that randomly
+    chooses rock paper or scissor.
+    1. Write a function that gets random choice of option using Math.random.
+    2. Get the user input through the console.
+    3. Print out / log who the winner is.
+
+
+*/
+
+
+
+console.log("Hello world!");
